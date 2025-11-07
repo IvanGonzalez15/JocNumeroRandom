e9b681ffdde8ef618a9d7e3ec443d87503e8c7a3
Ivan Gonzalez <ivantaranilla15@gmail.com>
2025-11-07 15:40:47 +0100

    NivelDos

--- src/main/java/exerciciwhilenumrandom/Main.java
+++ src/main/java/exerciciwhilenumrandom/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner llegirScanner = new Scanner (System.in);
 
         int numrandom = (int) (Math.random() * 11);
+        int numrandom2 = (int) (Math.random() * 101);
         int numero;
 
 
@@ -21,6 +22,22 @@ public class Main {
             }
 
         } while (numero != numrandom);
+
+        System.out.println("Felisidade mi loco, te ganaste este par de ñemita");
+
+        System.out.println("NIVEL DO");
+
+        do { 
+            System.out.println("Escriu un numero del 1 al 100");
+            numero = llegirScanner.nextInt();
+
+            if (numero == numrandom2) {
+                System.out.println("LOCOWIN LOCOWIN LOCOWIN LOCOWIN LOCOWIN LOCOWIN");
+            } else {
+                System.out.println("Tens un altre oportunitat");
+            }
+
+        } while (numero != numrandom2);
 
         llegirScanner.close();
     }
